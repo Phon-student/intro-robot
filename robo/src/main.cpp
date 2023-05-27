0a50f3cec4fdd8f293274fcdccbeb400caaa756c
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2023-05-28 02:29:42 +0700

    [==] add

--- robo/src/main.cpp
+++ robo/src/main.cpp
@@ -1,148 +1,261 @@
 #include <Arduino.h>
-#include <pinout.h>
-#include <config.h>
+#include "config.h"
+#include "pinout.h"
 
-#define isINVERTED_X false
-#define isINVERTED_Y false
-#define isINVERTED_Z false
+#define isInvertX true
+#define isInvertY false
+#define isInvertZ true
 
-#define limitAngle_X 180
-#define limitAngle_Y 180
-#define limitAngle_Z 180
+#define limitAngleX 180
+#define limitAngleY 180
+#define limitAngleZ 180
 
-#define delayTime 500
+#define delayTime 1000
 
-char inputString[100];
+char recieve;
 
-struct Motor{
+struct Motor {
   int stepPin;
   int dirPin;
-  int enablePin;
+  int enPin;
   int switchPin;
-  bool isINVERTED;
+  int steppos =0;
+  bool isInvert;
+  float anglePerStep = 0.50625; // 0.50625 degree per step (1.8 degree per step) (1/16 micro step)(2:9 gear ratio)
+  float angle = steppos * 1.975; // 1/0.50625 step per degree
   float limitAngle;
-  float angle = 0;
 };
 
 Motor motor[3];
 
-void limitCheck(int i){
-  if(motor[i].angle > motor[i].limitAngle){
-    motor[i].angle = motor[i].limitAngle;
+void limitor(int motorNo) {
+  if (motor[motorNo].angle > motor[motorNo].limitAngle) {
+    Serial.println("Rotate Over LIMIT!!!");
+    Serial.println("System Stop");
+    while (1);
   }
-  if(motor[i].angle < 0){
-    motor[i].angle = 0;
+  if (motor[motorNo].angle < 0) {
+    Serial.println("Rotate Over LIMIT!!!");
+    Serial.println("System Stop");
+    while (1);
   }
 }
 
-void setAngle(int motorNo, float Finalangle){
-  float offset = Finalangle - motor[motorNo].angle;
-  if(offset != 0){
-    if(offset > 0){
-      digitalWrite(motor[motorNo].dirPin, HIGH);
-    }
-    else{
-      digitalWrite(motor[motorNo].dirPin, LOW);
-    }
-    for(int i = 0; i<abs(offset); i++){
-      digitalWrite(motor[motorNo].stepPin, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(motor[motorNo].stepPin, LOW);
-      delayMicroseconds(delayTime);
-    }
-    motor[motorNo].angle = Finalangle;
+void homeX(){
+  int i = 0;
+  digitalWrite(motor[i].enPin, LOW);
+  if (motor[i].isInvert)digitalWrite(motor[i].dirPin, LOW);
+  else digitalWrite(motor[i].dirPin, HIGH);
+
+  while (!digitalRead(motor[i].switchPin)) {
+    digitalWrite(motor[i].stepPin, HIGH);
+    delayMicroseconds(delayTime);
+    digitalWrite(motor[i].stepPin, LOW);
+    delayMicroseconds(delayTime);
   }
-  for(int  i=0 ; i<offset ; i++){
+  motor[i].angle = 0; 
+  Serial.println("motor " + String(i) + "homed");
+}
+
+void homeY(){
+  int i = 1;
+  digitalWrite(motor[i].enPin, LOW);
+  if (motor[i].isInvert)digitalWrite(motor[i].dirPin, LOW);
+  else digitalWrite(motor[i].dirPin, HIGH);
+
+  while (!digitalRead(motor[i].switchPin)) {
+    digitalWrite(motor[i].stepPin, HIGH);
+    delayMicroseconds(delayTime);
+    digitalWrite(motor[i].stepPin, LOW);
+    delayMicroseconds(delayTime);
+  }
+  motor[i].angle = 0;
+  Serial.println("motor " + String(i) + "homed");
+}
+
+void homeZ(){
+  int i = 2;
+  digitalWrite(motor[i].enPin, LOW);
+  if (motor[i].isInvert)digitalWrite(motor[i].dirPin, LOW);
+  else digitalWrite(motor[i].dirPin, HIGH);
+
+  while (!digitalRead(motor[i].switchPin)) {
+    digitalWrite(motor[i].stepPin, HIGH);
+    delayMicroseconds(delayTime);
+    digitalWrite(motor[i].stepPin, LOW);
+    delayMicroseconds(delayTime);
+  }
+  motor[i].angle = 0;
+  Serial.println("motor " + String(i) + "homed");
+}
+
+void setHome() {
+  homeX();
+  homeY();
+  homeZ();
+}
+// void move(int motorNo, float Targetsteppos){
+//   while (Targetsteppos < motor[motorNo].steppos) {  
+//     digitalWrite(motor[motorNo].dirPin, !motor[motorNo].isInvert);
+//     digitalWrite(motor[motorNo].stepPin, HIGH);
+//     digitalWrite(motor[motorNo].stepPin, LOW);
+//     motor[motorNo].steppos--;
+//     delayMicroseconds(delayTime);
+//     Serial.println(motor[motorNo].steppos);
+//   }
+  
+//   while (Targetsteppos > motor[motorNo].steppos) {    
+//     digitalWrite(motor[motorNo].dirPin, !motor[motorNo].isInvert);
+//     digitalWrite(motor[motorNo].stepPin, HIGH);
+//     digitalWrite(motor[motorNo].stepPin, LOW);;
+//     digitalWrite(motor[motorNo].stepPin, LOW);
+//     motor[motorNo].steppos++;
+//     delayMicroseconds(delayTime);
+//     Serial.println(motor[motorNo].steppos);
+//   }
+// }
+
+void move(int motorNo, float targetAngle){
+  float Targetsteppos = targetAngle / motor[motorNo].anglePerStep;
+  while (Targetsteppos < motor[motorNo].steppos) {  
+    digitalWrite(motor[motorNo].dirPin, !motor[motorNo].isInvert);
     digitalWrite(motor[motorNo].stepPin, HIGH);
     digitalWrite(motor[motorNo].stepPin, LOW);
+    motor[motorNo].steppos--;
     delayMicroseconds(delayTime);
-    if(offset > 0){
-      motor[motorNo].angle++;
-    }
-    else{
-      motor[motorNo].angle--;
-    }
+    Serial.println(motor[motorNo].steppos);
   }
-  
+  while (Targetsteppos > motor[motorNo].steppos) {    
+    digitalWrite(motor[motorNo].dirPin, motor[motorNo].isInvert);
+    digitalWrite(motor[motorNo].stepPin, HIGH);
+    digitalWrite(motor[motorNo].stepPin, LOW);;
+    digitalWrite(motor[motorNo].stepPin, LOW);
+    motor[motorNo].steppos++;
+    delayMicroseconds(delayTime);
+    Serial.println(motor[motorNo].steppos);
+  }
 }
 
-
-
-void DH_matrix(float theta, float d, float a, float alpha, float T[4][4]){
-  T[0][0] = cos(theta);
-  T[0][1] = -sin(theta)*cos(alpha);
-  T[0][2] = sin(theta)*sin(alpha);
-  T[0][3] = a*cos(theta);
-
-  T[1][0] = sin(theta);
-  T[1][1] = cos(theta)*cos(alpha);
-  T[1][2] = -cos(theta)*sin(alpha);
-  T[1][3] = a*sin(theta);
-
-  T[2][0] = 0;
-  T[2][1] = sin(alpha);
-  T[2][2] = cos(alpha);
-  T[2][3] = d;
-
-  T[3][0] = 0;
-  T[3][1] = 0;
-  T[3][2] = 0;
-  T[3][3] = 1;
+void setAngle(int motorNo, float finalAngle) {
+  digitalWrite(motor[0].enPin, LOW);
+  digitalWrite(motor[1].enPin, LOW);
+  digitalWrite(motor[2].enPin, LOW);
+  float offSet = finalAngle - motor[motorNo].angle;
+  int offSetM;
+  if (offSet < 0 ) {
+    if (motor[motorNo].isInvert)digitalWrite(motor[motorNo].dirPin, LOW);
+    else digitalWrite(motor[motorNo].dirPin, HIGH);
+    Serial.println("Negative Offset");
+    offSetM = -offSet;
+  } else {
+    if (motor[motorNo].isInvert)digitalWrite(motor[motorNo].dirPin, HIGH);
+    else digitalWrite(motor[motorNo].dirPin, LOW);
+    Serial.println("Positive Offset");
+    offSetM = offSet;
+  }
+  for (int i = 0; i < (offSetM / 0.025); i++) {
+    digitalWrite(motor[motorNo].stepPin, HIGH);
+    delayMicroseconds(delayTime);
+    digitalWrite(motor[motorNo].stepPin, LOW);
+    delayMicroseconds(delayTime);
+    if (offSet < 0 ) {
+      motor[motorNo].angle -= 0.025;
+//      Serial.println(motor[motorNo].angle);
+      if (motor[motorNo].angle < 0.025) {
+        Serial.println("Rotate Over LIMIT!!!");
+        Serial.println("System Stop");
+        break;
+      }
+    }
+    else {
+      motor[motorNo].angle += 0.025;
+      Serial.println(motor[motorNo].angle);
+      if (motor[motorNo].angle > motor[motorNo].limitAngle-0.025) {
+        Serial.println("Rotate Over LIMIT!!!");
+        Serial.println("System Stop");
+        break;
+      }
+    }
+  }
+  digitalWrite(motor[0].enPin, HIGH);
+  digitalWrite(motor[1].enPin, HIGH);
+  digitalWrite(motor[2].enPin, HIGH);
 }
 
+void setAllAngle(float finalX, float finalY, float finalZ){
+  setAngle(0, finalX);
+  setAngle(1, finalY);
+  setAngle(2, finalZ);
+}
 
-void Homeset(){
-  for(int i = 0; i<3; i++){
-    while(digitalRead(motor[i].switchPin) == 1){
-      digitalWrite(motor[i].dirPin, HIGH);
-      digitalWrite(motor[i].stepPin, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(motor[i].stepPin, LOW);
-      delayMicroseconds(delayTime);
-    }
-    motor[i].angle = 0;
-  }
-
+void testSwitch(){
+    Serial.print(digitalRead(motor[0].switchPin));
+  Serial.print("\t");
+  Serial.print(digitalRead(motor[1].switchPin));
+  Serial.print("\t");
+  Serial.println(digitalRead(motor[2].switchPin));
+  Serial.println("-----------------------------");
+  delay(500);
 }
 
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Start");
   motor[0].stepPin = X_STEP_PIN;
   motor[0].dirPin = X_DIR_PIN;
-  motor[0].enablePin = X_ENABLE_PIN;
+  motor[0].enPin = X_ENABLE_PIN;
   motor[0].switchPin = X_MIN_PIN;
-  motor[0].isINVERTED = isINVERTED_X;
-  motor[0].limitAngle = limitAngle_X;
+  motor[0].isInvert = isInvertX;
+  motor[0].limitAngle = limitAngleX;
 
   motor[1].stepPin = Y_STEP_PIN;
   motor[1].dirPin = Y_DIR_PIN;
-  motor[1].enablePin = Y_ENABLE_PIN;
+  motor[1].enPin = Y_ENABLE_PIN;
   motor[1].switchPin = Y_MIN_PIN;
-  motor[1].isINVERTED = isINVERTED_Y;
-  motor[1].limitAngle = limitAngle_Y;
+  motor[1].isInvert = isInvertY;
+  motor[1].limitAngle = limitAngleY;
 
   motor[2].stepPin = Z_STEP_PIN;
   motor[2].dirPin = Z_DIR_PIN;
-  motor[2].enablePin = Z_ENABLE_PIN;
+  motor[2].enPin = Z_ENABLE_PIN;
   motor[2].switchPin = Z_MIN_PIN;
-  motor[2].isINVERTED = isINVERTED_Z;
-  motor[2].limitAngle = limitAngle_Z;
-
-  for(int i = 0; i<3; i++){
+  motor[2].isInvert = isInvertZ;
+  motor[2].limitAngle = limitAngleZ;
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  for (int i = 0; i < 3; i++) {
     pinMode(motor[i].stepPin, OUTPUT);
     pinMode(motor[i].dirPin, OUTPUT);
-    pinMode(motor[i].enablePin, OUTPUT);
+    pinMode(motor[i].enPin, OUTPUT);
     pinMode(motor[i].switchPin, INPUT);
-    digitalWrite(motor[i].enablePin, LOW);
   }
+  Serial.print("System Started");
+  for (int i = 0 ; i < 5; i++) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.println("Setting Home...");
+  setHome();
+  Serial.println("System Ready");
 
-  Serial.println("system ready");
-  Homeset();
-  
 }
 
-void loop(){
-
+void loop() {
+  if(Serial.available()){
+    recieve = Serial.read();
+    if(recieve == 'x'){
+      move(0, 45);
+      delay(1000);
+      move(1, 45);
+      delay(1000);
+      move(2, 45);
+        //print all angle
+      Serial.print(motor[0].steppos);
+      Serial.print("\t");
+      Serial.print(motor[0].steppos);
+      Serial.print("\t");
+      Serial.println(motor[0].steppos);
+      Serial.println("-----------------------------");
+    }
+    delay(100);
+  }
 }
